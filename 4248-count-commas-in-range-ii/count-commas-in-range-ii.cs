9683edaba1940f851918f5136ef4a82aa52568e1
ShravanKumarChiluka <108580553+ShravanKumarChiluka@users.noreply.github.com>
2026-09-09 22:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (11.11%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public long CountCommas(long n)
+    {
+        long count = 0;
+
+        long start = 1000;
+        long commas = 1;
+
+        while (start <= n)
+        {
+            long end = start * 1000 - 1;
+
+            if (end > n)
+            {
+                end = n;
+            }
+
+            count += (end - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return count;
+    }
+}
